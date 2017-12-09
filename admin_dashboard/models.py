68106ad9d64210ba7d94caefd15a6bd925e13bd2
feat7 <vinaykhobragade99@gmail.com>
2017-12-09 18:31:40 +0530

    Population migrated

--- admin_dashboard/models.py
+++ admin_dashboard/models.py
@@ -43,6 +43,27 @@ class Place(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Population(models.Model):
+        YES = 'YES'
+        NO = 'NO'
+
+        name = models.CharField(max_length=200)
+        total_population = models.BigIntegerField()
+        tribal_population = models.BigIntegerField()
+        percent_tribal_population = models.IntegerField()
+        
+        status = models.CharField(
+            max_length=20,
+            choices=(
+                (YES, 'YES'),
+                (NO, 'NO'),
+            ),
+            default=NO
+        )
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
+        
+
     def __str__(self):
         return     
 
