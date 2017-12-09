50b59bf2df9b55c2e1ca68d95a98feea81c61544
feat7 <vinaykhobragade99@gmail.com>
2017-12-09 18:15:42 +0530

    Scheme migrations done

---
+++ admin_dashboard/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Scheme',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('name', models.CharField(max_length=200)),
+                ('description', models.TextField()),
+                ('used_amount', models.FloatField()),
+                ('allocated_amount', models.FloatField()),
+                ('status', models.CharField(choices=[('YES', 'YES'), ('NO', 'NO')], default='NO', max_length=20)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+    ]
