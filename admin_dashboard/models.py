979a012d4706d63a773b0e2f4048701b92b481f2
feat7 <vinaykhobragade99@gmail.com>
2017-12-09 18:49:44 +0530

    Announcement+Complaint migrated

--- admin_dashboard/models.py
+++ admin_dashboard/models.py
@@ -93,6 +93,46 @@ class Allocation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+class Complaint(models.Model):
+    YES = 'YES'
+    NO = 'NO'
+
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    allocation_id = models.ForeignKey(
+        'Allocation',
+        on_delete=models.CASCADE
+    )
+    
+    status = models.CharField(
+        max_length=20,
+        choices=(
+            (YES, 'YES'),
+            (NO, 'NO'),
+        ),
+        default=NO
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Announcement(models.Model):
+    YES = 'YES'
+    NO = 'NO'
+
+    name = models.TextField()
+    description = models.TextField()    
+    
+    status = models.CharField(
+        max_length=20,
+        choices=(
+            (YES, 'YES'),
+            (NO, 'NO'),
+        ),
+        default=NO
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return     
 
