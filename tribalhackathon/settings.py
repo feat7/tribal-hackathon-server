d61420eab68f4cd6224a2a4f1982194a6fad0e3d
feat7 <vinaykhobragade99@gmail.com>
2017-12-09 20:27:39 +0530

    Middleware_classes fix

--- tribalhackathon/settings.py
+++ tribalhackathon/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'admin_dashboard'
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
